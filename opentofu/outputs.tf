c9f90e2c9ce76ee0faabead8b61fc05280673ad8
Chris Ackerman <ac.chris.ackerman@gmail.com>
2024-08-23 13:30:11 +0200

    Adding unifi commons outputs

--- opentofu/outputs.tf
+++ opentofu/outputs.tf
@@ -1,4 +1,29 @@
 # Terraform Outputs
-output "test" {
-  value = var.vlans
+output "Unifi-Network-VLANs" {
+  value = unifi_network.vlans
+}
+
+output "Unifi-Firewall-Groups" {
+  value = unifi_firewall_group.firewall-groups
+}
+
+output "Unifi-Firewall-Rules" {
+  value = unifi_firewall_rule.vlan-firewall-rules
+}
+
+output "Unifi-User-Groups" {
+  value = unifi_user_group.user-groups
+}
+
+output "Unifi-Users" {
+  value = unifi_user.users
+}
+
+output "Unifi-Default-AP-Group" {
+  value = data.unifi_ap_group.default
+}
+
+output "Unifi-WiFi-Configs" {
+  sensitive = true
+  value     = unifi_wlan.wifi
 }
