65db4e377934888645644266daa252bbcafad322
Chris Ackerman <ac.chris.ackerman@gmail.com>
2024-08-23 11:33:41 +0200

    adding provider configs and the unifi provider

--- opentofu/providers.tf
+++ opentofu/providers.tf
@@ -13,9 +13,47 @@ terraform {
       source  = "maxlaverse/bitwarden"
       version = "~> 0.8.0"
     }
+    unifi = {
+      source  = "ubiquiti-community/unifi"
+      version = "0.41.2"
+    }
     null = {
       source  = "hashicorp/null"
       version = "3.2.2"
     }
   }
+  backend "azurerm" {
+    storage_account_name = var.azure-state-storage-account-name
+    container_name       = var.azure-state-storage-container-name
+    key                  = var.azure-state-storage-key
+    use_azuread_auth     = true
+    subscription_id      = var.azure-state-storage-subscription-id
+    tenant_id            = var.azure-state-storage-tenant-id
+    client_id            = var.azure-state-storage-client-id
+    client_secret        = var.azure-state-storage-client-secret
+  }
+}
+
+provider "azurerm" {
+  features {}
+  environment         = "Public"
+  storage_use_azuread = true
+}
+
+provider "azuread" {
+
+}
+
+provider "bitwarden" {
+  email           = var.bitwarden-email
+  master_password = var.bitwarden-master-password
+  client_id       = var.bitwarden-client-id
+  client_secret   = var.bitwarden-client-secret
+}
+
+provider "unifi" {
+  username       = data.bitwarden_item_login.unifi-credentials.username
+  password       = data.bitwarden_item_login.unifi-credentials.password
+  api_url        = var.unifi-api-url
+  allow_insecure = var.unifi-insecure
 }
