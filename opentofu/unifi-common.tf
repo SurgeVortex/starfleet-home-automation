97f5726275833296632cb2acfd5be592fe8878ab
Chris Ackerman <ac.chris.ackerman@gmail.com>
2024-08-23 10:53:32 +0200

    Finalizing unifi-common resources and variables

--- opentofu/unifi-common.tf
+++ opentofu/unifi-common.tf
@@ -1,71 +1,55 @@
-# # Main Terraform Configuration
-# locals {
-#   storage-container-secrets = { for field in data.bitwarden_item_login.storage-container-connection-details.field : field.name => field.text }
-#   state-storage-user-secrets = { for field in data.bitwarden_item_login.azure-state-storage-user.field : field.name => field.text }
-# }
-
-# data "bitwarden_item_login" "storage-container-connection-details" {
-#   search     = "${var.}-connection-details"
-#   depends_on = [bitwarden_item_login.azure-state-storage-container-details]
-# }
-
-# data "bitwarden_item_login" "azure-state-storage-user" {
-#   search     = azuread_service_principal.state-storage-service-principal.display_name
-#   depends_on = [bitwarden_item_login.azure-state-storage-user]
-# }
-
 locals {
   firewall-rules-allow-to = flatten([
-    for key, value in var.vlans : [
+    for key, value in var.unifi-vlans : [
       for group in value.networks-allow-to : {
-        key            = "lan-out-allow-${key}-to-${group}"
-        name           = "Allow from ${value.name} to ${var.vlans[group].name}"
-        action         = "accept"
-        ruleset        = "LAN_OUT"
-        rule_index     = "40${value.id + 3}${index(var.vlans[key].networks-allow-to, group)}"
-        protocol       = "all"
+        key                    = "lan-out-allow-${key}-to-${group}"
+        name                   = "Allow from ${value.name} to ${var.unifi-vlans[group].name}"
+        action                 = "accept"
+        ruleset                = "LAN_OUT"
+        rule_index             = "40${value.id + 3}${index(var.unifi-vlans[key].networks-allow-to, group)}"
+        protocol               = "all"
         src_firewall_group_ids = [unifi_firewall_group.firewall-groups[key].id]
         dst_firewall_group_ids = [unifi_firewall_group.firewall-groups[group].id]
       }
     ]
   ])
   firewall-rules-allow-from = flatten([
-    for key, value in var.vlans : [
+    for key, value in var.unifi-vlans : [
       for group in value.networks-allow-from : {
-        key            = "lan-in-allow-${group}-to-${key}"
-        name           = "Allow from ${var.vlans[group].name} to ${value.name}"
-        action         = "accept"
-        ruleset        = "LAN_IN"
-        rule_index     = "40${value.id + 4}${index(var.vlans[key].networks-allow-from, group)}"
-        protocol       = "all"
+        key                    = "lan-in-allow-${group}-to-${key}"
+        name                   = "Allow from ${var.unifi-vlans[group].name} to ${value.name}"
+        action                 = "accept"
+        ruleset                = "LAN_IN"
+        rule_index             = "40${value.id + 4}${index(var.unifi-vlans[key].networks-allow-from, group)}"
+        protocol               = "all"
         src_firewall_group_ids = [unifi_firewall_group.firewall-groups[group].id]
         dst_firewall_group_ids = [unifi_firewall_group.firewall-groups[key].id]
       }
     ]
   ])
   firewall-rules-deny-to = flatten([
-    for key, value in var.vlans : [
+    for key, value in var.unifi-vlans : [
       for group in value.networks-deny-to : {
-        key            = "lan-out-deny-${key}-to-${group}"
-        name           = "Deny from ${value.name} to ${var.vlans[group].name}"
-        action         = "drop"
-        ruleset        = "LAN_OUT"
-        rule_index     = "40${value.id + 2}${index(var.vlans[key].networks-deny-to, group)}"
-        protocol       = "all"
+        key                    = "lan-out-deny-${key}-to-${group}"
+        name                   = "Deny from ${value.name} to ${var.unifi-vlans[group].name}"
+        action                 = "drop"
+        ruleset                = "LAN_OUT"
+        rule_index             = "40${value.id + 2}${index(var.unifi-vlans[key].networks-deny-to, group)}"
+        protocol               = "all"
         src_firewall_group_ids = [unifi_firewall_group.firewall-groups[key].id]
         dst_firewall_group_ids = [unifi_firewall_group.firewall-groups[group].id]
       }
     ]
   ])
   firewall-rules-deny-from = flatten([
-    for key, value in var.vlans : [
+    for key, value in var.unifi-vlans : [
       for group in value.networks-deny-from : {
-        key            = "lan-in-deny-${group}-to-${key}"
-        name           = "Deny from ${var.vlans[group].name} to ${value.name}"
-        action         = "drop"
-        ruleset        = "LAN_IN"
-        rule_index     = "40${value.id + 1}${index(var.vlans[key].networks-deny-from, group)}"
-        protocol       = "all"
+        key                    = "lan-in-deny-${group}-to-${key}"
+        name                   = "Deny from ${var.unifi-vlans[group].name} to ${value.name}"
+        action                 = "drop"
+        ruleset                = "LAN_IN"
+        rule_index             = "40${value.id + 1}${index(var.unifi-vlans[key].networks-deny-from, group)}"
+        protocol               = "all"
         src_firewall_group_ids = [unifi_firewall_group.firewall-groups[group].id]
         dst_firewall_group_ids = [unifi_firewall_group.firewall-groups[key].id]
       }
@@ -88,7 +72,7 @@ data "bitwarden_item_login" "unifi-credentials" {
 }
 
 resource "unifi_network" "vlans" {
-  for_each = var.vlans
+  for_each = var.unifi-vlans
   name     = each.value.name
   purpose  = each.value.purpose
 
@@ -103,7 +87,7 @@ resource "unifi_network" "vlans" {
 }
 
 resource "unifi_firewall_group" "firewall-groups" {
-  for_each   = var.vlans
+  for_each   = var.unifi-vlans
   name       = each.key
   type       = "address-group"
   members    = [each.value.cidr]
@@ -111,14 +95,47 @@ resource "unifi_firewall_group" "firewall-groups" {
 }
 
 resource "unifi_firewall_rule" "vlan-firewall-rules" {
-  for_each       = local.firewall-rules
-  name           = each.value.name
-  action         = each.value.action
-  ruleset        = each.value.ruleset
-  rule_index     = each.value.rule_index
-  protocol       = each.value.protocol
+  for_each               = local.firewall-rules
+  name                   = each.value.name
+  action                 = each.value.action
+  ruleset                = each.value.ruleset
+  rule_index             = each.value.rule_index
+  protocol               = each.value.protocol
   src_firewall_group_ids = each.value.src_firewall_group_ids
   dst_firewall_group_ids = each.value.dst_firewall_group_ids
 }
 
+resource "unifi_user_group" "user-groups" {
+  for_each          = var.unifi-user-groups
+  name              = each.key
+  qos_rate_max_down = each.value.qos_rate_max_down
+  qos_rate_max_up   = each.value.qos_rate_max_up
+}
 
+resource "unifi_user" "users" {
+  for_each        = var.unifi-trusted-clients
+  mac             = each.value.mac
+  name            = each.value.name
+  note            = try(each.value.note, null)
+  fixed_ip        = try(each.value.fixed_ip, null)
+  dev_id_override = try(each.value.device_id, 0)
+  network_id      = unifi_network.vlans[each.value.network_name].id
+  user_group_id   = unifi_user_group.user-groups[each.value.network_name].id
+}
+
+data "unifi_ap_group" "default" {
+}
+
+resource "unifi_wlan" "wifi" {
+  for_each           = var.unifi-wlans
+  name               = each.value.ssid
+  security           = each.value.security
+  passphrase         = each.value.passphrase
+  network_id         = unifi_network.vlans[each.value.network_name].id
+  is_guest           = each.value.is_guest
+  mac_filter_enabled = each.value.mac_filter_enabled
+  mac_filter_list    = [for mac in var.unifi-trusted-clients : mac.mac if mac.network_name == each.value.network_name]
+  mac_filter_policy  = each.value.mac_filter_policy
+  ap_group_ids       = [data.unifi_ap_group.default.id]
+  user_group_id      = unifi_user_group.user-groups[each.value.network_name].id
+}
--- opentofu/variables.tf
+++ opentofu/variables.tf
@@ -1,39 +1,182 @@
 # Terraform Variables
-variable "vlans" {
+variable "bitwarden-email" {
+  description = "Email to use to log into BitWarden."
+  type        = string
+}
+
+variable "bitwarden-master-password" {
+  description = "Password to log into bitwarden to."
+  type        = string
+}
+
+variable "bitwarden-client-id" {
+  description = "Bitwarden API Client ID"
+  type        = string
+}
+
+variable "bitwarden-client-secret" {
+  description = "Bitwarden API Client Secret"
+  type        = string
+}
+
+# variable "bitwarden-organization" {
+#   description = "BitWarden Organization that secrets live in"
+#   type        = string
+#   default     = "starfleet-home-automation"
+# }
+
+
+variable "azure-state-storage-account-name" {
+  description = "Storage Account name that contains the container to store state files in."
+  type        = string
+}
+
+variable "azure-state-storage-container-name" {
+  description = "Azure Storage Container to store the state files in."
+  type        = string
+}
+
+variable "azure-state-storage-key" {
+  description = "Name of the state storage file."
+  type        = string
+}
+
+variable "azure-state-storage-subscription-id" {
+  description = "Subscription ID containig the Azure Storage Container to store the state files in."
+  type        = string
+}
+
+variable "azure-state-storage-tenant-id" {
+  description = "Tenant ID containig the Azure Storage Container to store the state files in."
+  type        = string
+}
+
+variable "azure-state-storage-client-id" {
+  description = "Client ID containig the Azure Storage Container to store the state files in."
+  type        = string
+}
+
+variable "azure-state-storage-client-secret" {
+  description = "Subscription containig the Azure Storage Container to store the state files in."
+  type        = string
+}
+
+variable "unifi-credentials-name" {
+  description = ""
+  type        = string
+  default     = "Unifi Local Admin"
+}
+
+variable "unifi-api-url" {
+  description = ""
+  type        = string
+  default     = "https://unifi/"
+}
+
+variable "unifi-insecure" {
+  description = ""
+  type        = bool
+  default     = true
+}
+
+variable "unifi-vlans" {
   description = "Map of VLANs"
   type = map(object({
-    id          = number
-    name        = string
-    cidr        = string
-    gateway     = string
-    dhcp_start  = string
-    dhcp_end    = string
-    dns_servers = list(string)
-    ports       = list(number)
-    rate_limit  = bool
-    access = object({
-      internet       = bool
-      networks_allow = list(string)
-      networks_deny  = list(string)
-    })
+    id                  = number
+    name                = string
+    purpose             = string
+    cidr                = string
+    gateway             = string
+    dhcp-start          = string
+    dhcp-end            = string
+    dns-servers         = list(string)
+    pxe-server          = string
+    ports               = list(number)
+    rate-limit          = bool
+    internet            = bool
+    networks-allow-to   = list(string)
+    networks-allow-from = list(string)
+    networks-deny-to    = list(string)
+    networks-deny-from  = list(string)
   }))
 
   default = {
     management = {
-      id          = 10
-      name        = "Management VLAN"
-      cidr        = "192.168.10.0/24"
-      gateway     = "192.168.10.1"
-      dhcp_start  = "192.168.10.100"
-      dhcp_end    = "192.168.10.200"
-      dns_servers = ["1.1.1.1"]
-      ports       = [1]
-      rate_limit  = false
-      access = {
-        internet       = true
-        networks_allow = []
-        networks_deny  = []
-      }
+      id                  = 10
+      name                = "Management VLAN"
+      purpose             = "corporate"
+      cidr                = "192.168.10.0/24"
+      gateway             = "192.168.10.1"
+      dhcp-start          = "192.168.10.100"
+      dhcp-end            = "192.168.10.200"
+      dns-servers         = ["1.1.1.1"]
+      pxe-server          = "192.168.10.2"
+      ports               = [1]
+      rate-limit          = false
+      internet            = true
+      networks-allow-to   = []
+      networks-allow-from = []
+      networks-deny-to    = []
+      networks-deny-from  = []
+    }
+  }
+}
+
+variable "unifi-user-groups" {
+  description = "list of unifi user groups"
+  type = map(object({
+    qos_rate_max_down = number
+    qos_rate_max_up   = number
+  }))
+  default = {
+    "trusted" = {
+      qos_rate_max_down = -1
+      qos_rate_max_up   = -1
+    }
+  }
+}
+
+variable "unifi-trusted-clients" {
+  description = "Map of trusted clients"
+  type = map(object({
+    mac          = string
+    name         = string
+    note         = optional(string)
+    fixed_ip     = optional(string)
+    device_id    = optional(number)
+    network_name = string
+  }))
+  default = {
+    "Chris-Mac-Book-Pro-LAN" = {
+      mac          = "00:11:22:33:44:55"
+      name         = "Chris-Mac-Book-Pro-LAN"
+      network_name = "trusted"
+    }
+  }
+}
+
+variable "unifi-wlans" {
+  description = "Map of WiFi Configs"
+  type = map(object({
+    ssid               = string
+    security           = string
+    passphrase         = string
+    network_name       = string
+    is_guest           = bool
+    mac_filter_enabled = bool
+    mac_filter_policy  = string
+    l2_isolation       = bool
+  }))
+  default = {
+    "SurgeWiFiAP" = {
+      ssid               = "SurgeWiFiAP"
+      security           = "wpapsk"
+      passphrase         = "12345678"
+      network_name       = "trusted"
+      is_guest           = false
+      mac_filter_enabled = true
+      mac_filter_policy  = "allow"
+      l2_isolation       = false
     }
   }
 }
