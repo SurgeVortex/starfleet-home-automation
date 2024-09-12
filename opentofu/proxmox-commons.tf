0b466a269e735e42c4531951ed744e0f6f27cc3c
Chris Ackerman <ac.chris.ackerman@gmail.com>
2024-09-13 00:13:28 +0200

    Refactor SSH key setup in provisioner

--- opentofu/proxmox-commons.tf
+++ opentofu/proxmox-commons.tf
@@ -146,13 +146,12 @@ resource "null_resource" "setup_bastion" {
 
   provisioner "remote-exec" {
     inline = [
-      "mkdir -p ~/.ssh",
-      "if [ ! -f ~/.ssh/id_rsa ]; then echo '${data.bitwarden_item_login.ssh-credentials.notes}' > ~/.ssh/id_rsa; chmod 600 ~/.ssh/id_rsa; fi",
-      "if [ ! -f ~/.ssh/id_rsa.pub ]; then echo '${local.ssh-credentials-secrets.ssh-public-key}' > ~/.ssh/id_rsa.pub; chmod 644 ~/.ssh/id_rsa.pub; fi",
+      "if [ ! -f ~/.ssh/id_rsa ]; then echo '${nonsensitive(data.bitwarden_item_login.ssh-credentials.notes)}' > ~/.ssh/id_rsa; chmod 600 ~/.ssh/id_rsa; fi",
+      "if [ ! -f ~/.ssh/id_rsa.pub ]; then echo '${nonsensitive(local.ssh-credentials-secrets.ssh-public-key)}' > ~/.ssh/id_rsa.pub; chmod 644 ~/.ssh/id_rsa.pub; fi",
       "if ! grep -q 'BW_EMAIL' ~/.profile; then echo 'export BW_EMAIL=${data.bitwarden_item_login.bitwarden-api-credentials.username}' >> ~/.profile; fi",
-      "if ! grep -q 'BW_MASTER_PASSWORD' ~/.profile; then echo 'export BW_MASTER_PASSWORD=${data.bitwarden_item_login.bitwarden-api-credentials.password}' >> ~/.profile; fi",
-      "if ! grep -q 'BW_CLIENTID' ~/.profile; then echo 'export BW_CLIENTID=${local.bw-api-credentials-secrets.client-id}' >> ~/.profile; fi",
-      "if ! grep -q 'BW_CLIENTSECRET' ~/.profile; then echo 'export BW_CLIENTSECRET=${local.bw-api-credentials-secrets.client-secret}' >> ~/.profile; fi",
+      "if ! grep -q 'BW_MASTER_PASSWORD' ~/.profile; then printf 'export BW_MASTER_PASSWORD=%s\n' '${nonsensitive(data.bitwarden_item_login.bitwarden-api-credentials.password)}' >> ~/.profile; fi",
+      "if ! grep -q 'BW_CLIENTID' ~/.profile; then echo 'export BW_CLIENTID=${nonsensitive(local.bw-api-credentials-secrets.client-id)}' >> ~/.profile; fi",
+      "if ! grep -q 'BW_CLIENTSECRET' ~/.profile; then echo 'export BW_CLIENTSECRET=${nonsensitive(local.bw-api-credentials-secrets.client-secret)}' >> ~/.profile; fi",
       "if ! grep -q 'AZURE_TENANT_ID' ~/.profile; then echo 'export AZURE_TENANT_ID=${var.azure-state-storage-tenant-id}' >> ~/.profile; fi",
       "if ! grep -q 'AZURE_CLIENT_ID' ~/.profile; then echo 'export AZURE_CLIENT_ID=${var.azure-state-storage-client-id}' >> ~/.profile; fi",
       "if ! grep -q 'AZURE_CLIENT_SECRET' ~/.profile; then echo 'export AZURE_CLIENT_SECRET=${var.azure-state-storage-client-secret}' >> ~/.profile; fi",
