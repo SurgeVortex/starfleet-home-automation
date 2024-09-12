ec27decfdaa88f05822b9a70aa45fe7ffb5d36ed
Chris Ackerman <ac.chris.ackerman@gmail.com>
2024-09-12 12:21:40 +0200

    Refactor run-tofu.sh script

--- run-tofu.sh
+++ run-tofu.sh
@@ -1,13 +1,28 @@
 #!/bin/bash
+# Get the name of the script
+SCRIPT_NAME=$(basename "$0")
+
+# Check if the lock file exists
+if [ -f "/tmp/${SCRIPT_NAME}.lock" ]; then
+    echo "Script is already running. Exiting..."
+    exit 1
+fi
+
+# Create the lock file
+touch "/tmp/${SCRIPT_NAME}.lock"
 
 PARENT_DIR=${PWD}
-PRE_REQ_DIR="${PARENT_DIR}/opentofu"
-PLAN_FILE="${PRE_REQ_DIR}/.tf-plan"
-DESTROY=${1}
+OPENTOFU_DIR="${PARENT_DIR}/opentofu"
+PLAN_FILE="${OPENTOFU_DIR}/.tf-plan"
+BITWARDEN_DIR="${OPENTOFU_DIR}/.bitwarden"
 
 function cleanup {
   echo "Removing Plan File: ${PLAN_FILE}"
   rm  ${PLAN_FILE}
+  echo "Removing Lock File: /tmp/${SCRIPT_NAME}.lock"
+  rm "/tmp/${SCRIPT_NAME}.lock"
+#   echo "Removing Bitwarden Directory: ${BITWARDEN_DIR}"
+#   rm -rf ${BITWARDEN_DIR}
 }
 
 trap cleanup EXIT
@@ -40,11 +55,11 @@ then
     az login --allow-no-subscriptions --tenant "${AZURE_TENANT_ID}" --use-device-code
 fi
 
-cd "${PRE_REQ_DIR}"
+cd "${OPENTOFU_DIR}"
 
 tofu init -upgrade
 TYPE="apply"
-if [ "$(echo ${DESTROY} | tr '[:upper:]' '[:lower:]')" == "destroy" ]
+if [ "$(echo ${1} | tr '[:upper:]' '[:lower:]')" == "destroy" ]
 then
     TYPE="destroy"
     tofu plan -out=${PLAN_FILE} -destroy
@@ -56,7 +71,17 @@ then
     exit
 fi
 
-read -p "Continue with ${TYPE} [Yes/no]? " ANSWER
+if [ -n "$1" ]; then
+    if [ "$1" == "destroy" ]; then
+        TYPE="destroy"
+        read -p "Continue with ${TYPE} [Yes/no]? " ANSWER
+    elif [ "$1" == "auto-approve" ]; then
+        ANSWER="yes"
+    fi
+else
+    read -p "Continue with ${TYPE} [Yes/no]? " ANSWER
+fi
+
 ANSWER=$(echo ${ANSWER} | tr '[:upper:]' '[:lower:]')
 ANSWER=${ANSWER:-yes}
 
