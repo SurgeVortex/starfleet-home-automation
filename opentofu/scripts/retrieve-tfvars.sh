bfea97b914d7d77674275c7f21a28a846d1c0cb5
Chris Ackerman <ac.chris.ackerman@gmail.com>
2024-09-12 16:41:37 +0200

    Refactor retrieve-tfvars.sh script to improve Bitwarden integration

--- opentofu/scripts/retrieve-tfvars.sh
+++ opentofu/scripts/retrieve-tfvars.sh
@@ -2,4 +2,6 @@
 
 # Retrieve the terraform.tfvars file from Bitwarden
 bw login --apikey
-BW_SESSION=$(bw unlock --passwordenv BW_MASTER_PASSWORD --raw) bw list items | jq -r '.[] | select(.name | startswith("terraform.tfvars")) | .notes' > "${OPENTOFU_DIR}/terraform.tfvars"
+BW_SESSION=$(bw unlock --passwordenv BW_MASTER_PASSWORD --raw)
+bw --nointeraction --session sync
+bw --nointeraction --session ${BW_SESSION} list items | jq -r '.[] | select(.name | startswith("terraform.tfvars")) | .notes' > "${OPENTOFU_DIR}/terraform.tfvars"
