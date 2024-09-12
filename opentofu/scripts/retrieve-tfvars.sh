3cdeb23701b1f0ff8afef4dda7f3b2fb153aabf8
Chris Ackerman <ac.chris.ackerman@gmail.com>
2024-09-12 16:44:22 +0200

    Refactor retrieve-tfvars.sh script to fix Bitwarden session synchronization

--- opentofu/scripts/retrieve-tfvars.sh
+++ opentofu/scripts/retrieve-tfvars.sh
@@ -3,5 +3,5 @@
 # Retrieve the terraform.tfvars file from Bitwarden
 bw login --apikey
 BW_SESSION=$(bw unlock --passwordenv BW_MASTER_PASSWORD --raw)
-bw --nointeraction --session sync
+bw --nointeraction --session ${BW_SESSION} sync
 bw --nointeraction --session ${BW_SESSION} list items | jq -r '.[] | select(.name | startswith("terraform.tfvars")) | .notes' > "${OPENTOFU_DIR}/terraform.tfvars"
