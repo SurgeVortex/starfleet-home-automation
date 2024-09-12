60fb22d5c0125cf21a6af569f188707f2e4899ac
Chris Ackerman <ac.chris.ackerman@gmail.com>
2024-09-12 12:23:35 +0200

    Finalizing Proxmox VE configuration

---
+++ opentofu/proxmox-commons.tf
@@ -0,0 +1,147 @@
+locals {
+  ssh-credentials-secrets = { for field in data.bitwarden_item_login.ssh-credentials.field : field.name => field.text }
+}
+
+data "bitwarden_item_login" "proxmox-credentials" {
+  search = var.proxmox-credentials-name
+}
+
+data "bitwarden_item_login" "ssh-credentials" {
+  search = var.ssh-credentials-name
+}
+
+resource "proxmox_virtual_environment_cluster_options" "options" {
+  language   = "en"
+  keyboard   = "en-us"
+  email_from = "proxmox-root@isonet.casa"
+}
+
+resource "proxmox_virtual_environment_download_file" "cloud_images" {
+  for_each     = var.proxmox-cloud-images
+  content_type = each.value.content_type
+  datastore_id = each.value.datastore_id
+  node_name    = each.value.node_name
+  file_name    = "${each.key}.img"
+  url          = each.value.url
+}
+
+resource "proxmox_virtual_environment_vm" "virtual-machines" {
+  for_each  = var.proxmox-vms
+  name      = each.key
+  node_name = each.value.node_name
+  bios      = try(each.value.bios, "seabios")
+  cpu {
+    architecture = try(each.value.cpu.architecture, "x86_64")
+    cores        = 4
+    sockets      = 1
+  }
+  description = each.value.description
+  dynamic "disk" {
+    for_each = each.value.disk
+    content {
+      backup       = disk.value.backup
+      datastore_id = disk.value.datastore_id
+      file_id      = each.value.is_cloud_init ? proxmox_virtual_environment_download_file.cloud_images[disk.value.file_id].id : disk.value.file_id
+      interface    = disk.value.interface
+      size         = disk.value.size
+    }
+  }
+  dynamic "efi_disk" {
+    for_each = each.value.efi_disk != null ? [each.value.efi_disk] : []
+    content {
+
+
+      datastore_id      = efi_disk.datastore_id
+      file_format       = efi_disk.file_format
+      type              = efi_disk.type
+      pre_enrolled_keys = efi_disk.pre_enrolled_keys
+    }
+  }
+  dynamic "tpm_state" {
+    for_each = each.value.tpm_state != null ? [each.value.tpm_state] : []
+    content {
+
+
+      datastore_id = each.value.tpm_state.datastore_id
+      version      = each.value.tpm_state.version
+    }
+  }
+  dynamic "initialization" {
+    for_each = each.value.initialization != null ? { init = true } : each.value.is_cloud_init ? { cloud_init = true } : {}
+    content {
+      datastore_id = try(each.value.initialization.datastore_id != null ? each.value.initialization.datastore_id : "local-zfs", "local-zfs")
+      interface    = try(each.value.initialization.interface != null ? each.value.initialization.interface : "ide2", "ide2")
+      dynamic "dns" {
+        for_each = try(each.value.initialization.dns != null ? each.value.initialization : [], [])
+        content {
+          servers = dns.servers
+        }
+      }
+      ip_config {
+        ipv4 {
+          address = try(each.value.initialization.ip_config.ipv4.address != null ? each.value.initialization.ip_config.ipv4.address : "dhcp", "dhcp")
+          gateway = try(each.value.initialization.ip_config.ipv4.gateway, null)
+        }
+      }
+      dynamic "user_account" {
+        for_each = each.value.is_cloud_init ? ["cloud_init"] : each.value.initialization.user_account != null ? ["user_account"] : []
+        content {
+          keys     = each.value.is_cloud_init ? [local.ssh-credentials-secrets.ssh-public-key] : each.value.initialization.user_account.keys
+          username = each.value.is_cloud_init ? data.bitwarden_item_login.ssh-credentials.username : each.value.initialization.user_account.username
+          password = each.value.is_cloud_init ? data.bitwarden_item_login.ssh-credentials.password : each.value.initialization.user_account.password
+        }
+      }
+    }
+  }
+  keyboard_layout = try(each.value.keyboard_layout, "en-us")
+  memory {
+    dedicated = each.value.memory.dedicated
+    floating  = each.value.memory.floating
+    shared    = each.value.memory.shared
+  }
+  dynamic "network_device" {
+    for_each = each.value.network_device
+    content {
+      bridge  = try(network_device.value.bridge, "vmbr0")
+      model   = try(network_device.value.model, "virtio")
+      vlan_id = try(network_device.value.vlan_id, "40")
+    }
+  }
+  on_boot = each.value.on_boot
+  operating_system {
+    type = try(each.value.operating_system.type, "l26")
+  }
+  pool_id = each.value.pool_id
+  started = each.value.started
+  dynamic "startup" {
+    for_each = each.value.startup != null ? [each.value.startup] : []
+    content {
+      order = startup.value.order
+    }
+  }
+  vm_id = each.value.vm_id
+}
+
+resource "null_resource" "setup_bastion" {
+  for_each = try(proxmox_virtual_environment_vm.virtual-machines["bastion"] != null ? { bastion : "bastion" } : {}, {})
+  connection {
+    type        = "ssh"
+    host        = split("/", proxmox_virtual_environment_vm.virtual-machines["bastion"].initialization[0].ip_config[0].ipv4[0].address)[0]
+    user        = data.bitwarden_item_login.ssh-credentials.username
+    private_key = data.bitwarden_item_login.ssh-credentials.notes
+
+  }
+  triggers = {
+    script_checksum = md5(file("${path.module}/scripts/bastion-setup.sh"))
+  }
+  provisioner "file" {
+    source      = "${path.module}/scripts/bastion-setup.sh"
+    destination = "/tmp/script.sh"
+  }
+  provisioner "remote-exec" {
+    inline = [
+      "chmod +x /tmp/script.sh",
+      "/tmp/script.sh args",
+    ]
+  }
+}
---
+++ opentofu/proxmox-variables.tf
@@ -0,0 +1,103 @@
+variable "proxmox-credentials-name" {
+  description = "Credentials to log into Proxmox server."
+  type        = string
+  default     = "Proxmox Local Admin"
+}
+
+variable "ssh-credentials-name" {
+  description = "Credentials for Proxmox Instances."
+  type        = string
+  default     = "SSH Credentials"
+}
+
+variable "proxmox-cloud-images" {
+  description = "Cloud images to download."
+  type = map(object({
+    content_type = string
+    datastore_id = string
+    node_name    = string
+    url          = string
+  }))
+  default = {
+    "ubuntu_cloud_image" = {
+      content_type = "iso"
+      datastore_id = "local"
+      node_name    = "pve-1"
+      url          = "https://cloud-images.ubuntu.com/jammy/current/jammy-server-cloudimg-amd64.img"
+    }
+  }
+}
+
+variable "proxmox-vms" {
+  description = "Map of VMs"
+  default     = {}
+  type = map(object({
+    bios = optional(string)
+    cpu = object({
+      architecture = optional(string)
+      cores        = number
+      sockets      = number
+    })
+    description = optional(string)
+    device_id   = optional(number)
+    disk = map(object({
+      backup       = optional(bool)
+      datastore_id = string
+      file_id      = optional(string)
+      interface    = string
+      size         = number
+    }))
+    efi_disk = optional(object({
+      datastore_id      = optional(string)
+      file_format       = optional(string)
+      type              = optional(string)
+      pre_enrolled_keys = optional(bool)
+    }))
+    tpm_state = optional(object({
+      datastore_id = optional(string)
+      version      = optional(string)
+    }))
+    initialization = optional(object({
+      datastore_id = optional(string)
+      interface    = optional(string)
+      dns = optional(object({
+        servers = optional(list(string))
+      }))
+      ip_config = optional(object({
+        ipv4 = optional(object({
+          address = optional(string)
+          gateway = optional(string)
+        }))
+      }))
+      user_account = optional(object({
+        keys     = optional(list(string))
+        username = optional(string)
+        password = optional(string)
+      }))
+    }))
+    is_cloud_init   = optional(bool, false)
+    keyboard_layout = optional(string)
+    memory = object({
+      dedicated = number
+      floating  = optional(number, 0)
+      shared    = optional(number, 0)
+    })
+    name = string
+    network_device = map(object({
+      bridge  = optional(string, "vmbr0")
+      model   = optional(string, "virtio")
+      vlan_id = optional(number, "40")
+    }))
+    node_name = string
+    on_boot   = optional(bool, true)
+    operating_system = optional(object({
+      type = optional(string)
+    }))
+    pool_id = optional(string)
+    started = optional(bool, true)
+    startup = optional(object({
+      order = number
+    }))
+    vm_id = number
+  }))
+}
