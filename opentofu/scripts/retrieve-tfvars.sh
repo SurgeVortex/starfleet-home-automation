56483f8f629dd3e9f0af3079f189a85967634501
Chris Ackerman <ac.chris.ackerman@gmail.com>
2024-09-12 23:20:58 +0200

    Refactor retrieve-tfvars.sh script to handle empty terraform.tfvars content

--- opentofu/scripts/retrieve-tfvars.sh
+++ opentofu/scripts/retrieve-tfvars.sh
@@ -4,4 +4,15 @@
 bw login --apikey
 BW_SESSION=$(bw unlock --passwordenv BW_MASTER_PASSWORD --raw)
 bw --nointeraction --session ${BW_SESSION} sync
-bw --nointeraction --session ${BW_SESSION} list items | jq -r '.[] | select(.name | startswith("terraform.tfvars")) | .notes' > "${OPENTOFU_DIR}/terraform.tfvars"
+
+# Retrieve the terraform.tfvars content
+TFVARS_CONTENT=$(bw --nointeraction --session ${BW_SESSION} list items | jq -r '.[] | select(.name | startswith("terraform.tfvars")) | .notes')
+
+# Check if the content is empty
+if [ -z "$TFVARS_CONTENT" ]; then
+    echo "Error: terraform.tfvars not found in Bitwarden."
+    exit 1
+fi
+
+# Write the content to the file
+echo "$TFVARS_CONTENT" > "${OPENTOFU_DIR}/terraform.tfvars"
