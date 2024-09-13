a9f0301a9969b2bdbb139c3fac403c7fce14f406
Chris Ackerman <ac.chris.ackerman@gmail.com>
2024-09-13 13:37:48 +0200

    Added sensitive to the variables to keep logs cleaner

--- opentofu/outputs.tf
+++ opentofu/outputs.tf
@@ -1,26 +1,32 @@
 # Terraform Outputs
 output "Unifi-Network-VLANs" {
   value = unifi_network.vlans
+  sensitive = true
 }
 
 output "Unifi-Firewall-Groups" {
   value = unifi_firewall_group.firewall-groups
+  sensitive = true
 }
 
 output "Unifi-Firewall-Rules" {
   value = unifi_firewall_rule.vlan-firewall-rules
+  sensitive = true
 }
 
 output "Unifi-User-Groups" {
   value = unifi_user_group.user-groups
+  sensitive = true
 }
 
 output "Unifi-Users" {
   value = unifi_user.users
+  sensitive = true
 }
 
 output "Unifi-Default-AP-Group" {
-  value = data.unifi_ap_group.default
+  value     = data.unifi_ap_group.default
+  sensitive = true
 }
 
 output "Unifi-WiFi-Configs" {
