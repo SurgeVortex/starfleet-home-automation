2c141f3e17efe1d37795c5e448245b4fa820c556
Chris Ackerman <ac.chris.ackerman@gmail.com>
2024-09-13 17:41:40 +0200

    tofu format

--- opentofu/outputs.tf
+++ opentofu/outputs.tf
@@ -1,26 +1,26 @@
 # Terraform Outputs
 output "Unifi-Network-VLANs" {
-  value = unifi_network.vlans
+  value     = unifi_network.vlans
   sensitive = true
 }
 
 output "Unifi-Firewall-Groups" {
-  value = unifi_firewall_group.firewall-groups
+  value     = unifi_firewall_group.firewall-groups
   sensitive = true
 }
 
 output "Unifi-Firewall-Rules" {
-  value = unifi_firewall_rule.vlan-firewall-rules
+  value     = unifi_firewall_rule.vlan-firewall-rules
   sensitive = true
 }
 
 output "Unifi-User-Groups" {
-  value = unifi_user_group.user-groups
+  value     = unifi_user_group.user-groups
   sensitive = true
 }
 
 output "Unifi-Users" {
-  value = unifi_user.users
+  value     = unifi_user.users
   sensitive = true
 }
 
