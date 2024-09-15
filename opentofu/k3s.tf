2b0978f771f606c3a2427f3f6c91fdd3dc4bff78
Chris Ackerman <ac.chris.ackerman@gmail.com>
2024-09-15 11:03:06 +0200

    Refactor K3s installation to use local variables for command and options and added join cluster resource

--- opentofu/k3s.tf
+++ opentofu/k3s.tf
@@ -1,3 +1,8 @@
+locals {
+  k3s-install-command = "curl -sfL https://get.k3s.io | sh -s -"
+  k3s-install-options = "--tls-san=${var.k3s-controlplane-ip} --disable-cloud-controller --disable servicelb --disable local-storage  --disable traefik --node-name=$(hostname -f)"
+}
+
 resource "random_password" "k3s_secret" {
   length  = 16
   special = true
@@ -22,11 +27,12 @@ resource "null_resource" "install_k3s" {
     inline = [
       "export K3S_SECRET=${nonsensitive(random_password.k3s_secret.result)}",
       "export K3S_KUBECONFIG_MODE=${var.k3s-config-mode}",
-      "curl -sfL https://get.k3s.io | sh -s - server --cluster-init --tls-san=${var.k3s-controlplane-ip} --disable-cloud-controller --disable servicelb --disable local-storage  --disable traefik --node-name=$(hostname -f)"
+      "${local.k3s-install-command} server --cluster-init ${local.k3s-install-options}",
     #   k3sup join --ip 192.168.1.22 --server --server-ip 192.168.1.20 --server-user dmistry --sudo --k3s-extra-args "--disable traefik  --disable servicelb --node-ip=192.168.1.22"
     ]
   }
 }
+
 
 resource "null_resource" "install_kubevip" {
   for_each   = try(null_resource.install_k3s["k8s-master-1"] != null ? toset(["k8s-master-1"]) : toset([]), toset([]))
@@ -54,28 +60,26 @@ resource "null_resource" "install_kubevip" {
   }
 }
 
-# resource "null_resource" "join_k3s_nodes" {
-#   for_each   = { for vm in proxmox_virtual_environment_vm.virtual-machines: vm.name => vm if contains(lower(vm.name), "k8s") && !contains(lower(vm.name), "k8s-master-1") }
+resource "null_resource" "join_k3s_nodes" {
+  for_each   = { for vm in proxmox_virtual_environment_vm.virtual-machines: vm.name => vm if contains(lower(vm.name), "k8s") && !contains(lower(vm.name), "k8s-master-1") }
 
-#   depends_on = [null_resource.install_kubevip]
+  depends_on = [null_resource.install_kubevip]
 
-#   connection {
-#     type        = "ssh"
-#     host        = split("/", each.value.initialization[0].ip_config[0].ipv4[0].address)[0]
-#     user        = nonsensitive(data.bitwarden_item_login.ssh-credentials.username)
-#     private_key = nonsensitive(data.bitwarden_item_login.ssh-credentials.notes)
-#   }
+  connection {
+    type        = "ssh"
+    host        = split("/", each.value.initialization[0].ip_config[0].ipv4[0].address)[0]
+    user        = nonsensitive(data.bitwarden_item_login.ssh-credentials.username)
+    private_key = nonsensitive(data.bitwarden_item_login.ssh-credentials.notes)
+  }
 
-#   provisioner "remote-exec" {
-#     inline = [
-#             if [[ "${proxmox_vm_qemu.k3s_nodes[count.index].name}" == *"master"* ]]; then
-#                 curl -sfL https://get.k3s.io | K3S_TOKEN=${K3S_SECRET} sh -s - server --server ${K3S_URL}
-#             else
-#                 curl -sfL https://get.k3s.io | K3S_TOKEN=${K3S_SECRET} sh -s - agent --server ${K3S_URL}
-#             fi
-#      ]
-#   }
-# }
+  provisioner "remote-exec" {
+    inline = [
+      "export K3S_SECRET=${nonsensitive(random_password.k3s_secret.result)}",
+      "export K3S_KUBECONFIG_MODE=${var.k3s-config-mode}",
+      "${local.k3s-install-command} ${contains(lower(vm.name), "master") ? "server" : "agent"} ${local.k3s-install-options} --server https://${var.k3s-controlplane-ip}:6443",
+    ]
+  }
+}
 
 # resource "null_resource" "install_fluxcd" {
 #   depends_on = [null_resource.join_k3s_nodes]
