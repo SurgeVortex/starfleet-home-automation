d5d07dc4324b92a1a37e06e701c01db11e150b6e
Chris Ackerman <ac.chris.ackerman@gmail.com>
2024-09-16 21:29:19 +0200

    Update k3s installation command to use K3S_TOKEN instead of K3S_SECRET

--- opentofu/k3s.tf
+++ opentofu/k3s.tf
@@ -1,5 +1,5 @@
 locals {
-  k3s-install-command = "curl -sfL https://get.k3s.io | K3S_SECRET=${nonsensitive(random_password.k3s_secret.result)} K3S_KUBECONFIG_MODE=${var.k3s-config-mode} sh -s -"
+  k3s-install-command = "curl -sfL https://get.k3s.io | K3S_TOKEN=${nonsensitive(random_password.k3s_secret.result)} K3S_KUBECONFIG_MODE=${var.k3s-config-mode} sh -s -"
   k3s-install-options = "--tls-san=${var.k3s-controlplane-ip} --disable-cloud-controller --disable servicelb --disable local-storage  --disable traefik"
 }
 
