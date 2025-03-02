51484a6cc3f97519dca1007a87f9478c016faea9
Chris Ackerman <ac.chris.ackerman@gmail.com>
2025-03-02 13:55:55 +0200

    Downgrade unifi provider version from 0.41.2 to 0.41.1

--- opentofu/providers.tf
+++ opentofu/providers.tf
@@ -15,7 +15,7 @@ terraform {
     }
     unifi = {
       source  = "ubiquiti-community/unifi"
-      version = "0.41.2"
+      version = "0.41.1"
     }
     proxmox = {
       source  = "bpg/proxmox"
